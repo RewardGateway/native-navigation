c7ec83cda06d92cdda85ae80a86b82dedcea451a
Denis Dos Santos <denis.dossantos@rewardgateway.com>
2020-02-24 12:05:04 +0000

    androidx fixes

--- example/android/app/src/main/java/com/airbnb/android/react/navigation/example/MainActivity.java
+++ example/android/app/src/main/java/com/airbnb/android/react/navigation/example/MainActivity.java
@@ -5,7 +5,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.airbnb.android.react.navigation.ScreenCoordinatorLayout;
 import com.airbnb.android.react.navigation.ReactAwareActivity;
